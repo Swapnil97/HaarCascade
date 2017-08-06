313422dbf6fbe6a8abba86a76db5b726da5fe0f2
Roland Adams <roland.adams@smartideasllc.net>
2017-08-05 23:50:37 -0400

    Cleanded up some of the code

--- trainer.py
+++ trainer.py
@@ -30,14 +30,11 @@ def store_raw_images():
 	    with open("raw_neg/"+str(pic_num)+".jpg",'wb') as output:
 	    	output.write(imageData.read())  
 	#  img = cv2.imread(imageData,cv2.IMREAD_GRAYSCALE)
-
-            #img = cv2.imread("neg/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
-        #    urllib2.Request(i, "neg/"+str(pic_num)+".jpg")
 	    
-            img = cv2.imread("neg/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
+            img = cv2.imread("raw_neg/"+str(pic_num)+".jpg",cv2.IMREAD_GRAYSCALE)
             # should be larger than samples / pos pic (so we can place our image on it)
-#            resized_image = cv2.resize(img, (100, 100))
-#            cv2.imwrite("neg/"+str(pic_num)+".jpg",resized_image)
+            resized_image = cv2.resize(img, (100, 100))
+            cv2.imwrite("neg/"+str(pic_num)+".jpg",resized_image)
             pic_num += 1
 
         except Exception as e:
