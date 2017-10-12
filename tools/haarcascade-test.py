389fa5e1cc4f46ca8955c48c1f91001e1aea6132
SI Admin <siadmin@haarcascade.localdomain>
2017-10-11 22:25:37 -0400

    Changes for testing

--- tools/haarcascade-test.py
+++ tools/haarcascade-test.py
@@ -2,15 +2,17 @@ import numpy as np
 import cv2
 #face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-boob_cascade = cv2.CascadeClassifier('../data/haarcascade_breast_test_09202917_1022000.xml')
-img = cv2.imread('../raw_crop/4.jpg')
+boob_cascade = cv2.CascadeClassifier('../data/cascade.xml')
+img = cv2.imread('/home/siadmin/Downloads/black-and-red-iphone-7.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-boobs = boob_cascade.detectMultiScale(gray, 100, 100)
+boobs = boob_cascade.detectMultiScale(gray,scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30))
 
 print boobs
 for (x,y,w,h) in boobs:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
 cv2.imshow('img',img)
 cv2.waitKey(0)
