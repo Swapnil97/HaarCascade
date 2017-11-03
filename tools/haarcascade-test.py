b9d29806e1bbd5eacdab0813a2529fef6b7bf737
Roland Adams <roland.adams@smartideasllc.net>
2017-11-03 16:37:37 -0400

    New files and testing for twins

--- tools/haarcascade-test.py
+++ tools/haarcascade-test.py
@@ -1,18 +1,24 @@
 import numpy as np
 import cv2
 #face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-#eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+fullbody_cascade = cv2.CascadeClassifier('../data/haarcascade_fullbody.xml')
 boob_cascade = cv2.CascadeClassifier('../data/cascade.xml')
 img = cv2.imread('/home/siadmin/Downloads/black-and-red-iphone-7.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-boobs = boob_cascade.detectMultiScale(gray,scaleFactor=1.1,
+fullbody = fullbody_cascade.detectMultiScale(gray,scaleFactor=1.1,
     minNeighbors=5,
     minSize=(30, 30))
 
-print boobs
-for (x,y,w,h) in boobs:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+for(x,y,w,h) in fullbody:
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    boobs = boob_cascade.detectMultiScale(roi_gray,scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30))
+
+    for (bx,by,bw,bh) in boobs:
+        cv2.rectangle(roi_color,(bx,by),(bx+bw,by+bh),(255,0,0),2)
 
 cv2.imshow('img',img)
 cv2.waitKey(0)
